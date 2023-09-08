58d03711ef2f79070b84a7fa414c3b16f7b6b7a0
Zack Traczyk <20495250+xxzbuckxx@users.noreply.github.com>
2023-09-08 14:13:24 -0700

    remove landing email subscribe

--- src/views/Home/Landing/index.view.tsx
+++ src/views/Home/Landing/index.view.tsx
@@ -1,9 +1,6 @@
-import React, { useState } from "react"
-import { subscribeMailchimp } from "../../../utils/api"
-import { validateEmail } from "../../../utils/validate"
+import React from "react"
 import { ReactComponent as Moon } from "../../../assets/decorative/Moon.svg"
 import { ReactComponent as Sun } from "../../../assets/decorative/Sun.svg"
-import { ReactComponent as Arrow } from "../../../assets/icons/Arrow.svg"
 import { SocialButton } from "../../../components/Button/SocialButton"
 import { CalendarDaysIcon, MapPinIcon } from "@heroicons/react/24/outline"
 import {
@@ -14,42 +11,8 @@ import { useTheme } from "../../../contexts/ThemeContext/ThemeContext"
 
 import "./index.scss"
 
-const SubmissionStates = {
-  NotSubmitted: 0,
-  Loading: 1,
-  Submitted: 2,
-  Errored: 3,
-}
-
 const Landing: React.FC = () => {
-  const [email, setEmail] = useState<string>("")
-  const [state, setState] = useState<number>(0)
-  const [message, setMessage] = useState<string>("")
   const { theme } = useTheme()
-
-  const isLightClass = () => (theme.mode === "light" ? "--light" : "")
-
-  const handleSubmit = () => {
-    if (!validateEmail(email)) {
-      setState(SubmissionStates.Errored)
-      setMessage("Please enter a valid email address.")
-      return
-    }
-    setState(SubmissionStates.Loading)
-    setMessage("Submitting...")
-    subscribeMailchimp(email).then(res => {
-      if (res.status === 200 || res.status === 201) {
-        setState(SubmissionStates.Submitted)
-        setMessage(res.data.message)
-      } else {
-        setState(SubmissionStates.Errored)
-        setMessage(
-          "Oh no! We've got an error— please try your request again & contact" +
-            " us at dev@cruzhacks.com if this persists!",
-        )
-      }
-    })
-  }
 
   return (
     <div className='landing'>
@@ -84,36 +47,7 @@ const Landing: React.FC = () => {
             Stevenson Event Center @ UC Santa Cruz
           </div>
         </div>
-        <div className='landing__container--inputs'>
-          <div className='landing__container--inputs__row-container'>
-            <div className='landing__container--inputs__row-container__row1'>
-              <input
-                className='landing__container--inputs__row-container__row1--email-input'
-                onChange={e => setEmail(e.target.value)}
-                placeholder='Enter email for updates'
-              ></input>
-              <div
-                className={`landing__container--inputs__row-container__row1__arrow-container`}
-              >
-                <button
-                  className={`landing__container--inputs__row-container__row1__arrow-container--arrow${isLightClass()}`}
-                  onClick={() => handleSubmit()}
-                >
-                  <Arrow className='arrow-icon' />
-                </button>
-              </div>
-            </div>
-          </div>
-        </div>
-        {message && (
-          <div
-            className={
-              "landing__container--res" + (state === 3 ? "__error" : "")
-            }
-          >
-            {message}
-          </div>
-        )}
+        <div className='landing__container--inputs'></div>
       </div>
 
       <div className='landing__socials-mobile'>
