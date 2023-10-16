c2ce46de3bc1b4c149bc91a340791d769493c0cf
Sophie Han <32656696+sophiiehan@users.noreply.github.com>
2023-10-15 20:42:59 -0700

    Feature/sponsor us section (#17)
    Co-authored-by: Zack Traczyk <20495250+xxzbuckxx@users.noreply.github.com>

---
+++ src/views/Home/SupportSection/index.view.tsx
@@ -0,0 +1,65 @@
+import React from "react"
+import { ReactComponent as Instagram } from "../../../assets/icons/Instagram.svg"
+import { ReactComponent as Facebook } from "../../../assets/icons/Facebook.svg"
+import { ReactComponent as LinkedIn } from "../../../assets/icons/LinkedIn.svg"
+import { ReactComponent as Twitter } from "../../../assets/icons/Twitter.svg"
+import { ReactComponent as Discord } from "../../../assets/icons/Discord.svg"
+import "./index.scss"
+import { SocialButton } from "../../../components/Button/SocialButton"
+
+const SupportSection: React.FC = () => (
+  <div className='SupportSection__container'>
+    {/* support us! */}
+    <div className='SupportSection__container--SupportContainer'>
+      <div className='SupportSection__container--SupportContainer--title'>
+        SUPPORT US!
+      </div>
+      <div className='SupportSection__container--SupportContainer--icons'>
+        <SocialButton
+          logo={Instagram}
+          link={"https://www.instagram.com/cruzhacks/"}
+          key={"https://www.instagram.com/cruzhacks/"}
+        />
+        <SocialButton
+          logo={Facebook}
+          link={"https://www.facebook.com/CruzHacks/"}
+          key={"https://www.facebook.com/CruzHacks/"}
+        />
+        <SocialButton
+          logo={LinkedIn}
+          link={"https://www.linkedin.com/company/cruzhacks/"}
+          key={"https://www.linkedin.com/company/cruzhacks/"}
+        />
+        <SocialButton
+          logo={Twitter}
+          link={"https://twitter.com/CruzHacks"}
+          key={"https://twitter.com/CruzHacks"}
+        />
+        <SocialButton
+          logo={Discord}
+          link={"https://discord.gg/NBUHWbmaeb"}
+          key={"https://discord.gg/NBUHWbmaeb"}
+        />
+      </div>
+    </div>
+
+    {/* sponsor us! */}
+    <div className='SupportSection__container--SponsorContainer'>
+      <div className='SupportSection__container--SponsorContainer--title'>
+        SPONSOR US!
+      </div>
+      <div className='SupportSection__container--SponsorContainer--blurb'>
+        Interested in sponsoring CruzHacks for our upcoming hackathon? Connect
+        with our Sponsorshop Team for more information on how supporting
+        CruzHacks will benefit you!
+      </div>
+      <div className='SupportSection__container--SponsorContainer--button'>
+        <button type='button' className='ConnectButton'>
+          Connect here
+        </button>
+      </div>
+    </div>
+  </div>
+)
+
+export default SupportSection
--- src/views/Home/index.view.tsx
+++ src/views/Home/index.view.tsx
@@ -7,15 +7,16 @@ import Milestones from "./Milestones/index.view"
 import Tracks from "./Tracks/index.view"
 
 import "./index.scss"
-
 import PreviousHackathons from "./PreviousHackathons/index.view"
 import { PreviousHackathonProps } from "../../Props/PreviousHackathonProps/props"
+import SupportSection from "./SupportSection/index.view"
 
 const Home: React.FC = () => (
   <>
     <Landing />
     <div className='home__container'>
       <HeroBlurb />
+      <SupportSection />
       <Tracks />
       <Milestones />
       <PreviousHackathons hackathons={PreviousHackathonProps} />
